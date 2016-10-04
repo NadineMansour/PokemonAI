47602fa75180f2f48b482c30ca101efa8af7b587
Nadine Mansour <nadine.mansour94@gmail.com>
2016-10-04 14:36:59 +0200

    Added the goal test and the path cost function to the PokemonProblem class

--- src/dataStructures/PokemonsProblem.java
+++ src/dataStructures/PokemonsProblem.java
@@ -1,17 +1,28 @@
 package dataStructures;
 
+import maze.Cell;
+
 public class PokemonsProblem extends Problem {
+	
+	Cell destination;
+	
+	public PokemonsProblem(Cell destination) {
+		this.destination = destination;
+	}
 
 	@Override
 	boolean passTheGoalTest(State state) {
-		// TODO Auto-generated method stub
-		return false;
+		PokemonState pokemonState = (PokemonState)state;
+		if (pokemonState.timeToHatch == 0 && pokemonState.uncollectedPokemons.size() == 0 && pokemonState.location.equal(this.destination)) {
+			return true;
+		} else {
+			return false;
+		}
 	}
 
 	@Override
 	int pathCost(Node node) {
-		// TODO Auto-generated method stub
-		return 0;
+		return node.costFromRoot+1;
 	}
 
 }
